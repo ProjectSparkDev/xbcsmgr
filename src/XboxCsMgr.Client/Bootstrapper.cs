6599253fc688257c2933d392438384bd8a80c06d
soltani <ali.soltani2000@hotmail.com>
2024-09-28 00:12:44 +0330

    Fixed the bootstrap parsing issue some tokens were split between two wincred credentials. also removed the trailing X cutting because a token could have a X at the split point. should be addressed later.

--- src/XboxCsMgr.Client/Bootstrapper.cs
+++ src/XboxCsMgr.Client/Bootstrapper.cs
@@ -1,16 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using Stylet;
-using XboxCsMgr.Client.ViewModels;
-using XboxCsMgr.XboxLive;
-using XboxCsMgr.Helpers.Win32;
-using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using XboxCsMgr.XboxLive.Model.Authentication;
-using XboxCsMgr.XboxLive.Services;
 using System.Diagnostics;
 using System.Linq;
-using Newtonsoft.Json.Schema;
-using Newtonsoft.Json.Linq;
+using XboxCsMgr.Client.ViewModels;
+using XboxCsMgr.Helpers.Win32;
+using XboxCsMgr.XboxLive;
+using XboxCsMgr.XboxLive.Model.Authentication;
+using XboxCsMgr.XboxLive.Services;
 
 namespace XboxCsMgr.Client
 {
@@ -19,7 +17,7 @@ namespace XboxCsMgr.Client
         public static XboxLiveConfig? XblConfig { get; internal set; }
         private AuthenticateService authenticateService;
         private string DeviceToken { get; set; }
-        private string UserToken { get; set; } 
+        private string UserToken { get; set; }
 
         protected override void ConfigureIoC(StyletIoC.IStyletIoCBuilder builder)
         {
@@ -50,25 +48,65 @@ namespace XboxCsMgr.Client
             // Lookup current Xbox Live authentication data stored via wincred
             Dictionary<string, string> currentCredentials = CredentialUtil.EnumerateCredentials();
 
-            var xblCredentials = currentCredentials.Where(k => k.Key.Contains("Xbl|")
-                    && k.Key.Contains("Dtoken") 
+            var xblCredentials = currentCredentials.Where(k => k.Key.Contains("Xbl|") || k.Key.Contains("XblGrts|")
+                    && k.Key.Contains("Dtoken")
                     || k.Key.Contains("Utoken"))
                     .ToDictionary(p => p.Key, p => p.Value);
 
+            string PartialCredential = null;
+            // turns out my whole issue was that a token was split between two credentials. i had to do this to get both Dtoken and Utoken
+
             foreach (var credential in xblCredentials)
             {
-                // Remove trailing 'X' that is found on some credentials
-                var fixedJson = credential.Value.TrimEnd('X').ToString();
-                XboxLiveToken? token = JsonConvert.DeserializeObject<XboxLiveToken>(fixedJson);
-                if (token.TokenData.NotAfter > DateTime.UtcNow)
+                // there could be a X at the end of a token. so we wont remove it like the old code
+                //var fixedJson = credential.Value.TrimEnd('X').ToString();
+
+                var json = credential.Value;
+                XboxLiveToken? token = null;
+                try
                 {
-                    if (credential.Key.Contains("Dtoken"))
+                    token = JsonConvert.DeserializeObject<XboxLiveToken>(json);
+                }
+                catch (JsonReaderException ex1)
+                {
+                    //partial token merge here
+
+                    if (PartialCredential == null)
                     {
-                        DeviceToken = token.TokenData.Token;
+                        PartialCredential = json;
                     }
-                    else if (credential.Key.Contains("Utoken"))
+                    else
                     {
-                        if (token.TokenData.Token != "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") UserToken = token.TokenData.Token;
+                        try
+                        {
+                            token = JsonConvert.DeserializeObject<XboxLiveToken>(json + PartialCredential);
+                        }
+                        catch (JsonReaderException ex2)
+                        {
+                            try
+                            {
+                                token = JsonConvert.DeserializeObject<XboxLiveToken>(PartialCredential + json);
+                            }
+                            catch (JsonReaderException ex3)
+                            {
+                                PartialCredential = json;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (token != null)
+                {
+                    if (token.TokenData.NotAfter > DateTime.UtcNow)
+                    {
+                        if (credential.Key.Contains("Dtoken"))
+                        {
+                            DeviceToken = token.TokenData.Token;
+                        }
+                        else if (credential.Key.Contains("Utoken"))
+                        {
+                            if (token.TokenData.Token != "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") UserToken = token.TokenData.Token;
+                        }
                     }
                 }
             }
